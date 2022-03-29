bc81e2d9a94fff19122be0467ebe46e617e4a354
hedon954 <171725713@qq.com>
2022-03-29 14:09:57 +0800

    v1/update: add RemoveFieldFilter.

--- go2ch/filter/removefieldfilter.go
+++ go2ch/filter/removefieldfilter.go
@@ -1,1 +1,11 @@
 package filter
+
+// RemoveFiledFilter removes the listed fields.
+func RemoveFiledFilter(fields []string) FilterFunc {
+	return func(m map[string]interface{}) map[string]interface{} {
+		for _, field := range fields {
+			delete(m, field)
+		}
+		return m
+	}
+}
